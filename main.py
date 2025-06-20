0d865b5d2f66b9ff5656297c94fd9b8ba0404a99
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-20 17:59:15 -0300

    adicionado funcao para visualizar data atual

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ root.configure(background= '#1D1D1D')
 def get_hello():
     nome = os.getlogin()
     hello.config(text=f'Olá, {nome}@')
+def get_date():
+    data_atual =strftime('%a, %d %b %Y')
+    date.config(text=data_atual)
 
 hello = Label(root, bg='#1D1D1D', fg='#8E27EA', font=('Montserrat', 16))
 hello.pack()
 
+date = Label(root, bg='#1D1D1D', fg='#8E27EA', font=('Montserrat', 14))
+date.pack(pady=2)
+
+
 get_hello()
+get_date()
 
 
 root.mainloop()
