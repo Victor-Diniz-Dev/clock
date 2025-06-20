702f7b21042b0682e45e12cc74248841289ab7b0
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-20 17:58:24 -0300

    adicionado funcão para visualizar nome do usuario

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ root.maxsize(600, 320)
 root.minsize(600,320)
 root.configure(background= '#1D1D1D')
 
+def get_hello():
+    nome = os.getlogin()
+    hello.config(text=f'Olá, {nome}@')
+
+hello = Label(root, bg='#1D1D1D', fg='#8E27EA', font=('Montserrat', 16))
+hello.pack()
+
+get_hello()
+
+
 root.mainloop()
