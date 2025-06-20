7b4fe0e911c2d203ca9a97d1b89266ad2910db7a
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-20 17:57:21 -0300

    criação da janela

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+import os
+from time import strftime
+
+root = tk.Tk()
+root.title('Clock')
+root.geometry("600x320")
+root.maxsize(600, 320)
+root.minsize(600,320)
+root.configure(background= '#1D1D1D')
+
+root.mainloop()
